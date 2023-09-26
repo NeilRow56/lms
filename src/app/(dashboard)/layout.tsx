4c394873adc30f24d223536f3cf0fd5940c02044
NeilRow56 <neilrowland56@gmail.com>
2023-09-26 17:19:27 +0100

    Added Create course form

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from './_components/Navbar'
 import Sidebar from './_components/Sidebar'
+import ToasterProvider from '@/providers/ToastProvider'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,6 +11,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col  md:flex">
         <Sidebar />
       </div>
+      <ToasterProvider />
       <main className="h-full pt-[80px] md:pl-56">{children}</main>
     </div>
   )
